2f1d0ea0d6f4fb773d911df01343861f023298e0
Yuxi Lu <lucylulu12311@gmail.com>
2019-12-12 18:35:59 -0500

    setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+with open("Astraea/version.py", "r") as f:
+    exec(f.read())
+
+setup(name='Astraea',
+      version=__version__,
+      description='Predict rotation period of a star from various features',
+      url='https://github.com/lyx12311/Astraea',
+      author='Yuxi(Lucy) Lu',
+      author_email='lucylulu12311@gmail.com',
+      license=' ',
+      packages=['Astraea'],
+      install_requires=['numpy', 'pandas', 'astropy', 'sklearn'],
+      zip_safe=False)
