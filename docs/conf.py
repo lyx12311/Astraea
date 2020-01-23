40a2cecd1863c6cd1f764f66700c478a1c748072
Yuxi Lu <lucylulu12311@gmail.com>
2020-01-23 16:30:34 -0500

    build

--- docs/conf.py
+++ docs/conf.py
@@ -10,11 +10,37 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+dn = os.path.dirname
+sys.path.insert(0, dn(dn(dn(os.path.abspath(__file__)))))
 
 
+autodoc_mock_imports = [
+    "numpy",
+    "scikit-learn",
+    "pandas",
+    "astropy",
+    "matplotlib",
+]
+
+
+# convert tutorials
+import subprocess
+import glob
+import os
+
+for fn in glob.glob("_static/notebooks/*.ipynb"):
+    name = os.path.splitext(os.path.split(fn)[1])[0]
+    outfn = os.path.join("tutorials", name + ".rst")
+    print("Building {0}...".format(name))
+    subprocess.check_call(
+        "jupyter nbconvert --template tutorials/tutorial_rst --to rst "
+        + fn + " --output-dir tutorials", shell=True)
+    subprocess.check_call(
+        "python fix_internal_links.py " + outfn, shell=True)
+	
+	
 # -- Project information -----------------------------------------------------
 
 project = 'Astraea'
@@ -28,10 +54,17 @@ author = 'Yuxi (Lucy) Lu'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
+
+source_suffix = '.rst'
+master_doc = 'index'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -44,9 +77,21 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+pygments_style = 'sphinx'
+todo_include_todos = False
+
+import os
+on_rtd = os.environ.get("READTHEDOCS", None) == "True"
+if not on_rtd:
+    import sphinx_rtd_theme
+    html_theme = "sphinx_rtd_theme"
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+    
+    
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+htmlhelp_basename = 'Astraeadoc'
