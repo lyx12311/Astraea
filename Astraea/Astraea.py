b0a9deca662d2a5700b636ab4716befaeae9c14b
Yuxi Lu <lucylulu12311@gmail.com>
2020-01-27 17:35:11 -0500

    Added in extra calculations and tutorials

--- Astraea/Astraea.py
+++ Astraea/Astraea.py
@@ -26,7 +26,28 @@ def download_RF_regr_100est(url='https://zenodo.org/record/3620729/files/RF_Regr
     os.system('mv RF_Regre_model_1est_flicker.sav?download=1 ./data/RF_Regre_model_1est_flicker.sav')
 
 
+# load testing data
+def load_KeplerTest():
+    if not os.path.exists('./data/'):
+        os.system('mkdir data')
+    if not os.path.exists('./data/TestingData.pkl'):
+        os.system('mkdir data')
+        os.system('wget https://zenodo.org/record/3628778/files/TestingData.pkl?download=1')
+        os.system('mv TestingData.pkl?download=1 ./data/TestingData.pkl')
+    return pd.read_pickle('./data/TestingData.pkl')
+    
+
 def load_RF():
+    """ Load random forest classifier and regressors from zendo.org. 
+    
+    Two regressors will be laoded, one with 1 estimator and one with 100 estimators. 1 estimator minimizes
+    bias (systematic offset) with the cost of high varience (scattering) and 100 estimators minimizes varience and
+    maximizes bias. If user wants to predict rotation period from Kepler light curves then it is best to use model 
+    with 100 estimiators and user should use model with 1 estimator otherwise. Model trained on TESS light curves are
+    still being developed. 
+   
+    
+    """
     if not os.path.exists('./data/'):
         os.system('mkdir data')
     if not os.path.exists('./data/RF_Class_model.sav'):
@@ -46,16 +67,22 @@ def load_RF():
 
 """--------------------------------------------- start of function to use trained RF to get Prot ---------------------------------------------"""
 def FLICKERinstall():
+    """ Installs the FLICKER software to calcualte Flicker values from light curves. Documentation: https://flicker.readthedocs.io.
+    
+    """
     os.system('git clone https://github.com/lyx12311/FLICKER.git')
     os.system('cd FLICKER')
     os.system('python setup.py install')
     os.system('cd ..')
 
 def getTrainF():
-    Features_class=['LG_peaks [Lomb-Scargle peak height]', 'Rvar [ppm]', 'parallax [gaia]', 'radius_percentile_lower [gaia]', 'radius_percentile_upper [gaia]', 'phot_g_mean_flux_over_error [gaia]', 'bp_g [gaia]']
-    Features_regr=['teff [gaia]','bp_g [gaia]','lum_val [gaia]','v_tan [CalcV()]','phot_g_mean_flux_over_error [gaia]','v_b [CalcV()]','radius_val [gaia]','b [gaia]','Rvar [ppm]','flicker [FLICKER]']
-    print("classification features are:",Features_class)
-    print("regression features are:",Features_regr)
+    """Print out needed featuers for the model in order.
+    
+    """
+    Features_class=['LG_peaks', 'Rvar', 'parallax', 'radius_percentile_lower', 'radius_percentile_upper', 'phot_g_mean_flux_over_error', 'bp_g']
+    Features_regr=['teff','bp_g','lum_val','v_tan','phot_g_mean_flux_over_error','v_b','radius_val','b','Rvar','flicker']
+    print(">>> classification features are: ['LG_peaks [Lomb-Scargle peak height]', 'Rvar [ppm]', 'parallax [gaia]', 'radius_percentile_lower [gaia]', 'radius_percentile_upper [gaia]', 'phot_g_mean_flux_over_error [gaia]', 'bp_g [gaia]']\n\n")
+    print(">>> regression features are: ['teff [gaia]','bp_g [gaia]','lum_val [gaia]','v_tan [getVs()]','phot_g_mean_flux_over_error [gaia]','v_b [getVs()]','radius_val [gaia]','b [gaia]','Rvar [ppm]','flicker [FLICKER]']\n\n")
     return Features_class,Features_regr
 
 def getKeplerProt(X_pred):
@@ -67,13 +94,14 @@ def getKeplerProt(X_pred):
     the model which might take a couple of minues. It first passes the stars through a classifier, which identifies what stars 
     have measureable rotation periods. Then it uses two regressor models (one with 1 estimator and another one with 100 estimators) 
     to predict rotation periods. If column "Prot" exist, it will also output the true periods associated with the predicted periods.
-    The light curve feature "flicker" can be calculated using software `*FLICKER* <https://flicker.readthedocs.io/en/latest/>`_..
+    The light curve feature "flicker" can be calculated using software `FLICKER <https://flicker.readthedocs.io/en/latest/>`_..
     
     Args:
       X_pred ([Pandas DataFrame]): DataFrame contains all variables needed, run Astraea.getTrainF() to print out requirements
       
     Returns: 
-      <pandas.DataFrame> containing:
+      <pandas.DataFrame> or <pandas.Series>:
+      :Containing:
    
         :TrueProt: True rotation period (if avaliable)
 	:Prot_prediction_1est: Period predictions with 1 estimator
@@ -89,7 +117,7 @@ def getKeplerProt(X_pred):
         X_pred[Features_class]
 	
     except KeyError:
-        print("Make sure ['LG_peaks', 'Rvar_TESS', 'parallax', 'radius_percentile_lower', 'radius_percentile_upper', 'phot_g_mean_flux_over_error', 'bp_g'] are in DataFrame for classifier!")
+        print("Make sure ['LG_peaks', 'Rvar', 'parallax', 'radius_percentile_lower', 'radius_percentile_upper', 'phot_g_mean_flux_over_error', 'bp_g'] are in DataFrame for classifier!")
         print("Exiting...")
         return None
     
@@ -107,9 +135,13 @@ def getKeplerProt(X_pred):
     # get subset dropping NAs
     X_pred_class=X_pred[np.append(Features_regr,Features_class)].dropna()
     
+    # drop duplicate columns
+    X_pred_class=X_pred_class.loc[:,~X_pred_class.columns.duplicated()]
+    
     # classify first...
     print("Total "+str(len(X_pred))+" stars!")
     print('Classifing '+str(len(X_pred_class))+' stars!')
+    
     probs = RF_class.predict_proba(X_pred_class[Features_class])
     preds = probs[:,1]
     for i in range(len(preds)):
@@ -128,18 +160,23 @@ def getKeplerProt(X_pred):
     
     
     # predict rotation periods with 1 estimator
-    Prot_predictions_1est=RF_regr_1.predict(X_pred_regr.values())
+    Prot_predictions_1est=RF_regr_1.predict(X_pred_regr.values)
     
     # predict rotation periods with 100 estimator
-    Prot_predictions_100est=RF_regr_100.predict(X_pred_regr.values())
+    Prot_predictions_100est=RF_regr_100.predict(X_pred_regr.values)
+    
+    print("Finished!")
     
     try:
-        TrueProt=X_pred_class['Prot']
-        return pd.DataFrame(np.array((TrueProt, Prot_predictions_1est, Prot_predictions_100est)).T,columns=['True Prot','Prot prediction w/ 1 est','Prot prediction w/ 100 est'])	
+        TrueProt=X_pred.iloc[X_pred_class.index].Prot.values
+        try:
+            TrueProt_err=X_pred.iloc[X_pred_class.index].Prot_err.values
+            return pd.DataFrame(np.array((TrueProt,TrueProt_err, Prot_predictions_1est, Prot_predictions_100est)).T,columns=['True Prot','True Prot_err','Prot prediction w/ 1 est','Prot prediction w/ 100 est'])	
+        except:
+            return pd.DataFrame(np.array((TrueProt, Prot_predictions_1est, Prot_predictions_100est)).T,columns=['True Prot','Prot prediction w/ 1 est','Prot prediction w/ 100 est'])	
     except:
         print("Can't identify true rotation period! Does not exist or rename column to 'Prot'!")
         return pd.DataFrame(np.array((Prot_predictions_1est, Prot_predictions_100est)).T,columns=['Prot prediction w/ 1 est','Prot prediction w/ 100 est'])
-    
     
     
 
@@ -147,32 +184,86 @@ def getKeplerProt(X_pred):
 
 
 
+"""--------------------------------------------- start of calculating light curve statistics ---------------------------------------------"""
+def getRvar(Flux):
+    """Calculates light curve Rvar
+    
+    Args:
+      Flux: The light curve flux in ppm
+    
+    Returns:
+      Rvar ([float]): The variability of the light curve
+    """
+    return abs(np.percentile(Flux,95)-np.percentile(Flux,5))
+
+from astropy.timeseries import LombScargle
+def getLGpeak(t,sig,sig_err):
+    """Calculates the location of the highest peak and the maximum power of the hightest peak
+    
+    Args:
+      t: Time [days]
+      sig: Flux
+      sig_err: Flux error
+    
+    Returns:
+      :LG_Prot ([float]): The period calculated from Lomb-Scargle
+      :LG_peaks ([float]): The maximum peak height
+    """
+    
+    # lomb-scargle
+    # get frequencies
+    freq = np.linspace(1./abs(t[-1]-t[0]), 1./abs(t[1]-t[0])*0.5, 100000)
+    # periods 
+    ps = 1./freq
+    power = LombScargle(t,sig,sig_err).power(freq)
+    peaks = np.array([i for i in range(1, len(ps)-1) if power[i-1]<power[i] and power[i+1] < power[i]])
+
+    return ps[power == max(power[peaks])][0],max(power[peaks])
+
+# calcualte v_t, v_b by passing in a dataframe with parallax, pmra, pmdec, ra, dec
+def getVs(df):
+    """ Calculates tangential velocity (v_tan) and vertical velocity proximation (v_b).
+    
+    Args:
+      df ([Pandas DataFrame]): DataFrame contains columns 'parallax', 'pmra', 'pmdec', 'ra', 'dec', which are parallax, ra proper motion, dec propermotion, right ascension and declination, respectively  
+    
+    """
+    d = coord.Distance(parallax=np.array(df.parallax) * u.mas,allow_negative=True)
+    vra = (np.array(df.pmra)*u.mas/u.yr * d).to(u.km/u.s, u.dimensionless_angles())
+    vdec = (np.array(df.pmdec)*u.mas/u.yr * d).to(u.km/u.s, u.dimensionless_angles())
+    v_t=np.sqrt(np.power(vra,2.)+np.power(vdec,2.)) # vtan
+    # v_b as a proxy for v_z:
+    c = coord.SkyCoord(ra=np.array(df.ra)*u.deg, dec=np.array(df.dec)*u.deg, distance=d,
+	              pm_ra_cosdec=np.array(df.pmra)*u.mas/u.yr,
+	              pm_dec=np.array(df.pmdec)*u.mas/u.yr)
+    gal = c.galactic
+    v_b = (gal.pm_b * gal.distance).to(u.km/u.s, u.dimensionless_angles()) # vb
+    return v_t,v_b
+    
+    
+def getFlicker(t,sig):
+    """Calculates the Flicker value
+    
+    Args:
+      t: Time [days]
+      sig: Flux
+    
+    Returns:
+      :Flicker ([float]): Flicker value
+    """
+    try:
+        import FLICKER
+    except:
+        FLICKERinstall()
+        import FLICKER
+    return FLICKER.Flicker(t,sig)
 
 
-"""--------------------------------------------- start of function to predict rotation period from RF ---------------------------------------------"""
 
-
-"""--------------------------------------------- end of function to predict rotation period from RF ---------------------------------------------"""
-
-
+"""--------------------------------------------- end of calculating light curve statistics ---------------------------------------------"""
     
 
 """--------------------------------------------- star of functions not related to RF --------------------------------------------- """
-# calcualte v_t, v_b by passing in a dataframe with parallax, pmra, pmdec, ra, dec
-def CalcV(df):
-	d = coord.Distance(parallax=np.array(df.parallax) * u.mas,allow_negative=True)
-	vra = (np.array(df.pmra)*u.mas/u.yr * d).to(u.km/u.s, u.dimensionless_angles())
-	vdec = (np.array(df.pmdec)*u.mas/u.yr * d).to(u.km/u.s, u.dimensionless_angles())
-	v_t=np.sqrt(np.power(vra,2.)+np.power(vdec,2.)) # vtan
-	# v_b as a proxy for v_z:
-	c = coord.SkyCoord(ra=np.array(df.ra)*u.deg, dec=np.array(df.dec)*u.deg, distance=d,
-	                  pm_ra_cosdec=np.array(df.pmra)*u.mas/u.yr,
-	                  pm_dec=np.array(df.pmdec)*u.mas/u.yr)
-	gal = c.galactic
-	v_b = (gal.pm_b * gal.distance).to(u.km/u.s, u.dimensionless_angles()) # vb
-	return v_t,v_b
-	# print(vb)
-
 
 # calculates chisq
 def calcChi(TrueVal,PreVal,TrueVal_err):
--- docs/user/api.rst
+++ docs/user/api.rst
@@ -4,11 +4,25 @@
 
 API documentation
 =================
+.. autofunction:: Astraea.FLICKERinstall
+
+.. autofunction:: Astraea.getFlicker
+
 .. autofunction:: Astraea.getKeplerProt
+
+.. autofunction:: Astraea.getRvar
+
+.. autofunction:: Astraea.getLGpeak
+
+.. autofunction:: Astraea.getTrainF
+
+.. autofunction:: Astraea.getVs
 
 .. autofunction:: Astraea.RFclassifier
 
 .. autofunction:: Astraea.RFregressor
+
+.. autofunction:: Astraea.load_RF
 
 .. autofunction:: Astraea.plot_result
 
